169b90e1f459860e1405c82431153afe03944f6c
gzp <z.p.gaal.devel@gmail.com>
2020-02-05 17:38:33 +0100

    fix docker

--- docker/binary/start.sh
+++ docker/binary/start.sh
@@ -3,4 +3,6 @@
 # execute service only if git clone and build is enabled
 if [ "${GIT_TOKEN}" != "" ]; then
     /webapp/binary/shine-app /webapp/binary/secret.config.json
+else
+	 exec "$@"
 fi    
--- docker/docker_build.bat
+++ docker/docker_build.bat
@@ -1,2 +1,3 @@
+copy ..\secret.config.json binary\
 docker build -t shine --build-arg GIT_ACCESS_TOKEN=%1 . 
 docker_prune.bat
