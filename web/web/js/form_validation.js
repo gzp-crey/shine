0767eb1c4f0565949f7dae60437443f18bcb5fb2
gzp-crey <gzp@creygames.com>
2020-03-13 17:54:12 +0100

    fix register page

--- web/web/js/form_validation.js
+++ web/web/js/form_validation.js
@@ -1,11 +1,14 @@
-function is_valid(elementId) {
-    var element = document.querySelector('#' + elementId + ' input');
-    return element.validity.valid;
+function form_input(elementId) {
+    return document.querySelector('#' + elementId + ' input');
 }
 
-function set_valid(elementId, silent) {
-    //console.log('valid: ' + silent + ' ' + elementId);
-    var element = document.querySelector('#' + elementId + ' input');
+function form_value(elementId) {
+    return form_input(elementId).value
+}
+
+function form_set_valid(elementId, silent) {
+    console.log('valid: ' + silent + ' ' + elementId);
+    var element = form_input(elementId);
     element.classList.remove('is-invalid');
     if (silent) {
         element.classList.remove('is-valid');
@@ -21,9 +24,9 @@ function set_valid(elementId, silent) {
     }
 }
 
-function set_invalid(elementId, silent, error_msg) {
-    //console.log('invalid: ' + silent + ' ' + elementId);
-    var element = document.querySelector('#' + elementId + ' input');
+function form_set_invalid(elementId, silent, error_msg) {
+    console.log('invalid: ' + silent + ' ' + elementId);
+    var element = form_input(elementId);
     element.setCustomValidity(error_msg);
     element.classList.remove('is-valid');
     if (silent) {
